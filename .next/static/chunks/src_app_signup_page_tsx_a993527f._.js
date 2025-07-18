81a66916560221de3f3b5cd956577f431cce06a3
elsoie <117638391+elsoie@users.noreply.github.com>
2025-07-18 21:30:42 +0700

    Add .next/static/chunks/src_app_signup_page_tsx_a993527f._.js 1752849039834

---
+++ .next/static/chunks/src_app_signup_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_fed51edd._.js",
+  "static/chunks/src_cea0d04f._.js"
+],
+    source: "dynamic"
+});
